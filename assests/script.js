eb96b26b4c09f8c32f3c50c31968d4b4ab6371e4
MKish420 <meaghanmpeters@hotmail.com>
2022-06-28 18:59:58 -0400

    first commit html pseudo css js

---
+++ assests/script.js
@@ -0,0 +1,12 @@
+// variables to keep track of the game state
+//variables to refrence dom elements (document.getelement by id 
+//sound effect (yeah right)
+//click start hide that screen
+//bring up questions
+//click answer
+//if correct show correct
+//if wrong show wrong
+//if wrong show correct answer
+//finish screen
+//show highscore 
+//reset button
